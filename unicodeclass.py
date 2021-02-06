cc0a1a3b7d93c43589d81078a38ba9f357e046c1
arshkenia <62417212+arshkenia@users.noreply.github.com>
2021-02-06 21:11:40 +0530

    Add files via upload

---
+++ unicodeclass.py
@@ -0,0 +1,82 @@
+from unicodecode import uniclist
+
+class UnicodeClass(object):
+    def __init__(self, text , types ="text" ):
+        self.type = types
+        self.text = text
+        self.unilist = uniclist
+    
+    def getUnicodeString(self):
+        if self.type == "text":
+            output = ""
+            i = 0
+            textstr = ""
+            for char in self.text:
+                print(char)
+                i = i + 1
+                for sublist in self.unilist:
+                    if char == sublist[1]:
+                        print(char)
+                        output = output + sublist[0]
+                        print(output)
+            return output
+        else:
+            raise Exception("Not given correct type")
+    
+    def getUnicodeList(self):
+        if self.type == "text":
+            output = []
+            i = 0
+            textstr = ""
+            for char in self.text:
+                print(char)
+                i = i + 1
+                for sublist in self.unilist:
+                    if char == sublist[1]:
+                        print(char)
+                        output.append(sublist[0])
+                        print(output)
+            return output
+        else:
+            raise Exception("Not given correct type")
+
+    def getTextString(self):
+        if self.type == "unicode":
+            output = ""
+            i = 0
+            unicharacter = ""
+            for char in self.text:
+                i == i + 1
+                unicharacter = unicharacter + char
+                if i % 2 == 0:
+                    for sublist in self.unilist:
+                        if unicharacter == sublist[0]:
+                            output = output + sublist[1]
+                            unicharacter = ""
+            return output
+
+        else:
+            raise Exception("Not correct type")
+
+    def getTextList(self):
+        if self.type == "unicode":
+            output = []
+            i = 0
+            unicharacter = ""
+            for char in self.text:
+                i == i + 1
+                unicharacter = unicharacter + char
+                if i % 2 == 0:
+                    for sublist in self.unilist:
+                        if unicharacter == sublist[0]:
+                            output.append(sublist[1])
+                            unicharacter = ""
+            return output
+
+        else:
+            raise Exception("Not correct type")
+
+                    
+
+
+            
